d9dab05c095a408910ed9aaee4afd5778975d69c
Thiago Hersan <thiago.hersan+github@gmail.com>
2016-01-15 15:44:33 +0000

    routing server

---
+++ HackNights_01_RoutingServer/serve.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var fs = require('fs');
+
+var app = express();
+app.use(express.static('static', {index: 'index.html'}));
+
+var imageFiles = fs.readdirSync('images/');
+
+app.get('/cat', function(req, res){
+    var fName = '';
+    while(fName.substring(0,3) != 'cat'){
+        // grab a random image and check if file name starts with 'cat'
+        fName = imageFiles[Math.floor(Math.random()*imageFiles.length)];
+    }
+    res.sendfile('images/'+fName);
+});
+
+app.listen(8080, function(){});
